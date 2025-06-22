6e9c3e536129987e1ed5c31eba69944d64541ca2
yashasuttangi <yashuttangi@gmail.com>
2025-06-22 11:41:45 +0530

    feat: login and signup form components

--- client/src/components/auth/login/index.tsx
+++ client/src/components/auth/login/index.tsx
@@ -23,23 +23,43 @@ const Login = () => {
       <h2>Welcome to FakeStackOverflow!</h2>
       <h3>Please login to continue.</h3>
       {/* TODO: Task 1 - Correctly handle form submission */}
-      <form>
+      <form onSubmit={handleSubmit}>
         <h4>Please enter your username.</h4>
         {/* TODO: Task 1 - Add an input field for the username input.
         The input field should correctly update the displayed value when text
         is entered. Use the 'input-text' class for styling.
         */}
+        <input 
+          type='text'
+          className='input-text'
+          value={username}
+          onChange={(e) => {handleInputChange(e, 'username')}}
+          required
+        />
         <h4>Please enter your password.</h4>
         {/* TODO: Task 1 - Add an input field for the password input.
         The input field should correctly update the value when text
         is entered. Make sure that the password visibility is correctly toggled.
         Use the 'input-text' class for styling.
         */}
+        <input 
+          type={showPassword ? 'text' : 'password'}
+          className='input-text'
+          value={password}
+          onChange={(e) => {handleInputChange(e, 'password')}}
+          required
+        />
         <div className='show-password'>
           {/* TODO: Task 1 - Add a checkbox input field for the visibility toggle.
         The field should correctly update the password visibility when checked/unchecked.
         Use the id 'showPasswordToggle'. No styling class is required here.
         */}
+          <input 
+            type='checkbox'
+            id='showPasswordToggle'
+            checked={showPassword}
+            onChange={togglePasswordVisibility}
+          />
           <label htmlFor='showPasswordToggle'>Show Password</label>
         </div>
         <button type='submit' className='login-button'>
--- client/src/components/auth/signup/index.tsx
+++ client/src/components/auth/signup/index.tsx
@@ -23,28 +23,55 @@ const Signup = () => {
     <div className='container'>
       <h2>Sign up for FakeStackOverflow!</h2>
       {/* TODO: Task 1 - Correctly handle form submission */}
-      <form>
+      <form onSubmit={handleSubmit}>
         <h4>Please enter your username.</h4>
         {/* TODO: Task 1 - Add an input field for the username input.
         The input field should correctly update the displayed value when text
         is entered. Use the 'input-text' class for styling.
         */}
+        <input 
+          type='text'
+          className='input-text'
+          value={username}
+          onChange={(e) => {handleInputChange(e, 'username')}}
+          required
+        />
         <h4>Please enter your password.</h4>
         {/* TODO: Task 1 - Add an input field for the password input.
         The input field should correctly update the value when text
         is entered. Make sure that the password visibility is correctly toggled.
         Use the 'input-text' class for styling.
         */}
+        <input 
+          type={showPassword ? 'text' : 'password'}
+          className='input-text'
+          value={password}
+          onChange={(e) => {handleInputChange(e, 'password')}}
+          required
+        />
         {/* TODO: Task 1 - Add an input field for the password confirmation input.
         The input field should correctly update the value when text
         is entered. Make sure that the password visibility is correctly toggled.
         Use the 'input-text' class for styling.
         */}
+        <input 
+          type={showPassword ? 'text' : 'password'}
+          className='input-text'
+          value={passwordConfirmation}
+          onChange={(e) => {handleInputChange(e, 'confirmPassword')}}
+          required
+        />
         <div className='show-password'>
           {/* TODO: Task 1 - Add a checkbox input field for the visibility toggle.
         The field should correctly update the password visibility when checked/unchecked.
         Use the id 'showPasswordToggle'. No styling class is required here.
         */}
+          <input 
+            type='checkbox'
+            id='showPasswordToggle'
+            checked={showPassword}
+            onChange={togglePasswordVisibility}
+          />
           <label htmlFor='showPasswordToggle'>Show Password</label>
         </div>
         <button type='submit' className='login-button'>
